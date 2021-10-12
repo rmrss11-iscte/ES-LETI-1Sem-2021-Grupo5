393d0fa629e17617c5057604f2e8dcfcef2d6c6c
ajbbo1-iscte <ajbbo1@iscte-iul.pt>
2021-10-12 01:02:21 +0100

    vazio

--- teste/src/teste/dhbdfdf.java
+++ teste/src/teste/dhbdfdf.java
@@ -4,10 +4,7 @@ public class dhbdfdf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i;
-		i=10;
-		System.out.print(i);
-		System.out.print("Toma");
+		
 	}
 
 }
