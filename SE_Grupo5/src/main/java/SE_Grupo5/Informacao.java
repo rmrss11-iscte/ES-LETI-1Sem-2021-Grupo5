a7f9558a73f4f1945a1e00773e6f7e5d35cd5ade
rmrss11-iscte <rmrss11@iscte-iul.pt>
2021-11-15 18:12:37 +0000

    Display do ProductBacklog(Precisa de uma pequena alteração)

--- SE_Grupo5/src/main/java/SE_Grupo5/Informacao.java
+++ SE_Grupo5/src/main/java/SE_Grupo5/Informacao.java
@@ -30,10 +30,10 @@ public class Informacao extends JFrame {
 	 * Create the frame.
 	 */
 	public Informacao(Trello trelloApi, String trelloUtilizador) {
-		
+
 		this.trelloApi = trelloApi;
 		this.trelloUtilizador = trelloUtilizador;
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1033, 634);
 		contentPane = new JPanel();
@@ -41,69 +41,72 @@ public class Informacao extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel membersLabel = new JLabel("Membros do projeto:");
 		membersLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		membersLabel.setBounds(10, 45, 211, 43);
 		contentPane.add(membersLabel);
-		
+
 		JTextArea membersDisplay = new JTextArea(getMembers());
 		membersLabel.setLabelFor(membersDisplay);
 		membersDisplay.setEditable(false);
 		membersDisplay.setBounds(20, 82, 253, 152);
 		contentPane.add(membersDisplay);
-		
+
 		JLabel dataLabel = new JLabel("Data de Inicio:");
 		dataLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		dataLabel.setBounds(10, 245, 170, 32);
 		contentPane.add(dataLabel);
-		
+
 		JLabel dataDisplay = new JLabel(getDate());
 		dataLabel.setLabelFor(dataDisplay);
 		dataDisplay.setBounds(10, 276, 305, 72);
 		contentPane.add(dataDisplay);
-		
-		JLabel productBacklogLabel= new JLabel("Items do ProductBacklog:");
+
+		JLabel productBacklogLabel = new JLabel("Items do ProductBacklog:");
 		productBacklogLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		productBacklogLabel.setBounds(10, 359, 211, 28);
 		contentPane.add(productBacklogLabel);
-		
+
 		JTextArea productBacklogDisplay = new JTextArea(getProductBacklog());
 		productBacklogLabel.setLabelFor(productBacklogDisplay);
 		productBacklogDisplay.setBounds(10, 398, 274, 152);
 		contentPane.add(productBacklogDisplay);
 	}
-	
+
 	private String getMembers() {
-		
+
 		List<Board> boards = trelloApi.getMemberBoards(trelloUtilizador);
 		List<Member> members = trelloApi.getBoardMembers(boards.get(0).getId());
-		for(Member m: members) {
-			membersList = membersList + m.getFullName()+"\n";
+		for (Member m : members) {
+			membersList = membersList + m.getFullName() + "\n";
 		}
 		return membersList;
 	}
-	
+
 	private String getDate() {
 		List<Board> boards = trelloApi.getMemberBoards(trelloUtilizador);
 		List<Card> cards = trelloApi.getBoardCards(boards.get(0).getId());
 		Card projectCard = trelloApi.getBoardCard(boards.get(0).getId(), cards.get(0).getId());
 		Date dataInicio = projectCard.getDue();
 		return dataInicio.toString();
-		}
-	
+	}
+
 	private String getProductBacklog() {
 		List<Board> boards = trelloApi.getMemberBoards(trelloUtilizador);
-		List<TList> lists = trelloApi.getBoardLists(boards.get(0).getId());	
-		List<Card> listCards = trelloApi.getListCards(lists.get(2).getId());
-		for(Card c: listCards) {
-			productBacklogList = productBacklogList + c.getName()+"\n";
+		for (Board b : boards) {
+			List<TList> lists = trelloApi.getBoardLists(b.getId());
+			for (TList l : lists) {
+
+				if (!l.getName().contains("Planning") && !l.getName().contains("Review")
+						&& !l.getName().contains("Retrospective") && !l.getName().contains("Scrum")) {
+					List<Card> listCards = trelloApi.getListCards(l.getId());
+					for (Card c : listCards) {
+						productBacklogList += c.getName() + "\n";
+					}
+				}
+			}
 		}
 		return productBacklogList;
-		
 	}
-	}
-	
-	
-
-	
+}
