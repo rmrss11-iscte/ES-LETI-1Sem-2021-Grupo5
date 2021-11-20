fabd3222f0c4c1cd2b834256196bc73a98a85aad
ajbbo1-iscte <ajbbo1@iscte-iul.pt>
2021-11-20 00:33:25 +0000

    Recursos Humanos feito

--- SE_Grupo5/src/main/java/SE_Grupo5/GraficoBarra.java
+++ SE_Grupo5/src/main/java/SE_Grupo5/GraficoBarra.java
@@ -1,5 +1,6 @@
 package SE_Grupo5;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -9,6 +10,10 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+import java.awt.BorderLayout;
+
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 
 public class GraficoBarra extends JFrame{
 	
@@ -16,24 +21,156 @@ public class GraficoBarra extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 124479714253861621L;
-
-	public GraficoBarra(String title,String xLabel,String yLabel, List<GraficoData> data) {
+	JTabbedPane abas = new JTabbedPane();
+	
+	public GraficoBarra(String title,String xLabel,String yLabel, List<SprintHours> sprintHoursList) {
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
 		setTitle(title);
-		setSize(500, 500);
+		setSize(1000, 550);
 		setLocationRelativeTo(null);
-		criarGrafico(title,xLabel,yLabel,data);
+		add(BorderLayout.CENTER,abas);
+		
+		//adicionar aba com grafico do projeto
+		List<GraficoData> projetoData = new ArrayList<GraficoData>();
+		projetoData = getProjectHorasGraficoData(sprintHoursList);
+		abas.addTab("Projeto", criarGrafico("Horas de trabalho usadas no Projeto", "Membro da equipe", "Horas", projetoData));
+		
+		//adicionar abas com grafico dos sprints
+		for(SprintHours sprintHours : sprintHoursList) {
+			if (sprintHours.hasSpentTime()) {
+				
+				
+				List<GraficoData> sprintEstimateTimeData = new ArrayList<GraficoData>();
+				List<GraficoData> sprintSpentTimeData = new ArrayList<GraficoData>();
+				sprintEstimateTimeData = getSprintEstimateTimeGraficoData(sprintHours);
+				sprintSpentTimeData = getSprintSpentTimeGraficoData(sprintHours);
+				JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+				splitPane.setDividerSize(1);
+				splitPane.setDividerLocation(500);
+				splitPane.setLeftComponent(criarGrafico(sprintHours.getSprint() + " -Horas Estimadas", "Membro da equipe", "Horas",
+						sprintEstimateTimeData));
+				splitPane.setRightComponent(criarGrafico(sprintHours.getSprint() + " -Horas Usadas", "Membro da equipe", "Horas", sprintSpentTimeData));
+				abas.addTab(sprintHours.getSprint(),splitPane);
+			}
+		}
+		
+		setVisible(true);
+	}
+	public GraficoBarra(String title,String xLabel,String yLabel, List<SprintHours> sprintHoursList ,double custoHora) {
+		setDefaultCloseOperation(HIDE_ON_CLOSE);
+		setTitle(title);
+		setSize(550, 550);
+		setLocationRelativeTo(null);
+		add(BorderLayout.CENTER,abas);
+		
+		//adicionar aba com grafico do projeto
+		List<GraficoData> projetoData = new ArrayList<GraficoData>();
+		projetoData = getProjectPagamentoGraficoData(sprintHoursList,custoHora);
+		abas.addTab("Projeto", criarGrafico("Pagamento Total", "Membro da equipe", "Pagamento", projetoData));
+		
+		//adicionar abas com grafico dos sprints
+		for(SprintHours sprintHours : sprintHoursList) {
+			if (sprintHours.hasSpentTime()) {
+				List<GraficoData> sprintPagamentoData = new ArrayList<GraficoData>();
+				sprintPagamentoData = getSprintPagamentoGraficoData(sprintHours,custoHora);
+				abas.addTab(sprintHours.getSprint(),criarGrafico(sprintHours.getSprint(), "Membro da equipe", "Pagamento", sprintPagamentoData));
+			}
+		}
+		
 		setVisible(true);
 	}
 
-	private void criarGrafico(String title,String xLabel,String yLabel, List<GraficoData> data) {
+	private ChartPanel criarGrafico(String title,String xLabel,String yLabel, List<GraficoData> data) {
 		DefaultCategoryDataset barra = new  DefaultCategoryDataset();
 		for(GraficoData d: data) {
 			barra.setValue(d.yData, d.xData, "");
 		}
 		JFreeChart grafico = ChartFactory.createBarChart(title, xLabel, yLabel, barra, PlotOrientation.VERTICAL, true, true, true);
-		ChartPanel painel = new ChartPanel(grafico);
-		add(painel);
+		ChartPanel chartPanel = new ChartPanel(grafico);
+		return chartPanel;
+	}
+	
+	private List<GraficoData> getProjectHorasGraficoData(List<SprintHours> sprintHoursList) {
+
+		List<GraficoData> projetoData = new ArrayList<GraficoData>();
+
+		for (SprintHours sprintHours : sprintHoursList) {
+			if (sprintHours.hasSpentTime()) {
+				for (Hours hours : sprintHours.getHours()) {
+					int control = 0;
+					for (GraficoData graficoData : projetoData) {
+						if (graficoData.getXData().equals(hours.getUser())) {
+							graficoData.addYData(hours.getSpentTime());
+							control = 1;
+							break;
+						}
+					}
+					if (control == 0)
+						projetoData.add(new GraficoData(hours.getUser(), hours.getSpentTime()));
+				}
+			}
+		}
+
+		return projetoData;
 	}
 
+	private List<GraficoData> getProjectPagamentoGraficoData(List<SprintHours> sprintHoursList,double custoHora) {
+
+		List<GraficoData> projetoData = new ArrayList<GraficoData>();
+
+		for (SprintHours sprintHours : sprintHoursList) {
+			if (sprintHours.hasSpentTime()) {
+				for (Hours hours : sprintHours.getHours()) {
+					int control = 0;
+					for (GraficoData graficoData : projetoData) {
+						if (graficoData.getXData().equals(hours.getUser())) {
+							graficoData.addYData(hours.getSpentTime()*custoHora);
+							control = 1;
+							break;
+						}
+					}
+					if (control == 0)
+						projetoData.add(new GraficoData(hours.getUser(), hours.getSpentTime()*custoHora));
+				}
+			}
+		}
+
+		return projetoData;
+	}
+	
+	private List<GraficoData> getSprintEstimateTimeGraficoData(SprintHours sprintHours) {
+
+		List<GraficoData> sprintEstimateTimeData = new ArrayList<GraficoData>();
+
+		for (Hours hours : sprintHours.getHours()) {
+			sprintEstimateTimeData.add(new GraficoData(hours.getUser(), hours.getEstimateTime()));
+		}
+
+		return sprintEstimateTimeData;
+	}
+
+	private List<GraficoData> getSprintSpentTimeGraficoData(SprintHours sprintHours) {
+
+		List<GraficoData> sprintSpentTimeData = new ArrayList<GraficoData>();
+
+		for (Hours hours : sprintHours.getHours()) {
+			sprintSpentTimeData.add(new GraficoData(hours.getUser(), hours.getSpentTime()));
+		}
+
+		return sprintSpentTimeData;
+	}
+	
+	private List<GraficoData> getSprintPagamentoGraficoData(SprintHours sprintHours,double custoHora) {
+
+		List<GraficoData> sprintSpentTimeData = new ArrayList<GraficoData>();
+
+		for (Hours hours : sprintHours.getHours()) {
+			sprintSpentTimeData.add(new GraficoData(hours.getUser(), hours.getSpentTime()*custoHora));
+		}
+
+		return sprintSpentTimeData;
+	}
+	
+	
+	
 }
--- SE_Grupo5/src/main/java/SE_Grupo5/Informacao.java
+++ SE_Grupo5/src/main/java/SE_Grupo5/Informacao.java
@@ -11,6 +11,8 @@ import com.julienvey.trello.domain.*;
 
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,6 +20,12 @@ import java.awt.Color;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import java.awt.event.MouseAdapter;
+import javax.swing.JTextPane;
 
 public class Informacao extends JFrame {
 
@@ -25,11 +33,15 @@ public class Informacao extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 5924931067800093425L;
-	private JPanel contentPane;
+	private JPanel contentPane = new JPanel();
 	private Trello trelloApi;
 	private String trelloUtilizador;
 	private GitHub gitHubApi;
 	private List<SprintHours> sHours = new ArrayList<SprintHours>();
+	private JTextField txtNovoCustohora = new JTextField();;
+	private JTable tabelaHoras;
+	private JTable tabelaCusto;
+	private double custoHora = 20;
 
 	/**
 	 * Create the frame.
@@ -43,67 +55,125 @@ public class Informacao extends JFrame {
 		getProjectTime();
 
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
-		setBounds(100, 100, 1033, 634);
-		contentPane = new JPanel();
+		setBounds(100, 100, 952, 751);
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JLabel membersLabel = new JLabel("Membros do projeto:");
-		membersLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		membersLabel.setBounds(10, 45, 211, 43);
+		membersLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+		membersLabel.setBounds(10, 119, 211, 43);
 		contentPane.add(membersLabel);
 
 		JTextArea membersDisplay = new JTextArea(getMembers());
 		membersLabel.setLabelFor(membersDisplay);
 		membersDisplay.setEditable(false);
-		membersDisplay.setBounds(20, 82, 253, 152);
+		membersDisplay.setBounds(20, 162, 253, 152);
 		contentPane.add(membersDisplay);
 
 		JLabel dataLabel = new JLabel("Data de Inicio:");
-		dataLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		dataLabel.setBounds(10, 245, 170, 32);
+		dataLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+		dataLabel.setBounds(10, 310, 170, 32);
 		contentPane.add(dataLabel);
 
-		JLabel dataDisplay = new JLabel(getDate());
-		dataLabel.setLabelFor(dataDisplay);
-		dataDisplay.setBounds(10, 276, 305, 72);
-		contentPane.add(dataDisplay);
-
 		JLabel productBacklogLabel = new JLabel("Items do ProductBacklog:");
-		productBacklogLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		productBacklogLabel.setBounds(10, 359, 211, 28);
+		productBacklogLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+		productBacklogLabel.setBounds(10, 409, 263, 28);
 		contentPane.add(productBacklogLabel);
-
-		JTextArea productBacklogDisplay = new JTextArea(getProductBacklog());
-		productBacklogLabel.setLabelFor(productBacklogDisplay);
-		productBacklogDisplay.setBounds(10, 398, 274, 152);
-		contentPane.add(productBacklogDisplay);
-		JScrollPane scrollPane = CriarTabela(sHours);
-		scrollPane.setBounds(562, 45, 445, 152);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportBorder(null);
+		scrollPane.setBounds(20, 437, 274, 179);
 		contentPane.add(scrollPane);
+		
+				JTextArea productBacklogDisplay = new JTextArea(getProductBacklog());
+				scrollPane.setViewportView(productBacklogDisplay);
+				productBacklogLabel.setLabelFor(productBacklogDisplay);
 
-		criarGraficos();
+		JScrollPane scrollPane_custo = new JScrollPane();
+		scrollPane_custo.setBounds(444, 362, 445, 155);
+		contentPane.add(scrollPane_custo);
+		tabelaCusto = criarTabela(sHours, 20);
+		scrollPane_custo.setViewportView(tabelaCusto);
+
+		JLabel lblCustoHora = new JLabel("Custo-Hora =             €");
+		lblCustoHora.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblCustoHora.setBounds(587, 538, 203, 24);
+		contentPane.add(lblCustoHora);
+		txtNovoCustohora.setHorizontalAlignment(SwingConstants.CENTER);
+		txtNovoCustohora.setFont(new Font("Tahoma", Font.PLAIN, 18));
+
+		// txt Novo Custo-Hora
+		txtNovoCustohora.setText("20");
+		txtNovoCustohora.setBounds(699, 538, 67, 24);
+		contentPane.add(txtNovoCustohora);
+		txtNovoCustohora.setColumns(10);
+
+		JButton buttonApplyNovoCustoHora = new JButton("Apply");
+		buttonApplyNovoCustoHora.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setNovoCustoHora(Double.parseDouble(txtNovoCustohora.getText()));
+			}
+		});
+		buttonApplyNovoCustoHora.setBounds(800, 538, 89, 24);
+		contentPane.add(buttonApplyNovoCustoHora);
+
+		JScrollPane scrollPane_Horas = new JScrollPane();
+		scrollPane_Horas.setBounds(444, 160, 445, 155);
+		contentPane.add(scrollPane_Horas);
+		tabelaHoras = CriarTabela(sHours);
+		scrollPane_Horas.setViewportView(tabelaHoras);
+
+		JButton btnObterGraficoHoras = new JButton("Obter Gráficos das Horas de Trabalho");
+		btnObterGraficoHoras.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new GraficoBarra("Horas de trabalho", "Membro da equipe", "Horas", sHours);
+			}
+		});
+		btnObterGraficoHoras.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnObterGraficoHoras.setBounds(615, 573, 274, 43);
+		contentPane.add(btnObterGraficoHoras);
+
+		JButton btnObterGraficoCustos = new JButton("Obter Gráficos dos Custos do Trabalho");
+		btnObterGraficoCustos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				new GraficoBarra("Custos do Projeto", "Membro da equipe", "Euros", sHours, custoHora);
+			}
+		});
+		btnObterGraficoCustos.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnObterGraficoCustos.setBounds(615, 627, 274, 43);
+		contentPane.add(btnObterGraficoCustos);
+		
+		JLabel lblTabelaDeHoras = new JLabel("Tabela de horas previstas/usadas");
+		lblTabelaDeHoras.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblTabelaDeHoras.setBounds(412, 119, 324, 43);
+		contentPane.add(lblTabelaDeHoras);
+		
+		JLabel lblTabelaDeCustos = new JLabel("Tabela de custos");
+		lblTabelaDeCustos.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblTabelaDeCustos.setBounds(412, 326, 305, 43);
+		contentPane.add(lblTabelaDeCustos);
+		
+		JTextArea textDate = new JTextArea(getDate());
+		textDate.setBounds(20, 338, 211, 49);
+		contentPane.add(textDate);
+
+		
 
 	}
 
-	private void criarGraficos() {
-		List<GraficoData> projetoData = new ArrayList<GraficoData>();
-		projetoData = getProjectGraficoData();
-		new GraficoBarra("Horas de trabalho usadas no Projeto", "Membro da equipe", "Horas", projetoData);
-
-		for (SprintHours s : sHours) {
-			if (s.hasSpentTime()) {
-				List<GraficoData> sprintEstimateTimeData = new ArrayList<GraficoData>();
-				List<GraficoData> sprintSpentTimeData = new ArrayList<GraficoData>();
-				sprintEstimateTimeData = getSprintEstimateTimeGraficoData(s);
-				sprintSpentTimeData = getSprintSpentTimeGraficoData(s);
-				new GraficoBarra(s.getSprint() + " -Horas Estimadas", "Membro da equipe", "Horas",
-						sprintEstimateTimeData);
-				new GraficoBarra(s.getSprint() + " -Horas Usadas", "Membro da equipe", "Horas", sprintSpentTimeData);
-			}
+	private void setNovoCustoHora(double novoCustoHora) {
+		int row = 0;
+		while (row < tabelaCusto.getRowCount()) {
+			double newValue = ((Number) tabelaCusto.getValueAt(row, 2)).doubleValue() * (novoCustoHora / custoHora);
+			tabelaCusto.setValueAt(newValue, row, 2);
+			row++;
 		}
+		custoHora = novoCustoHora;
 	}
 
 	private String getMembers() {
@@ -126,8 +196,8 @@ public class Informacao extends JFrame {
 	}
 
 	private String getProductBacklog() {
-		
-		String productBacklogList="";
+
+		String productBacklogList = "";
 		List<Board> boards = trelloApi.getMemberBoards(trelloUtilizador);
 		for (Board b : boards) {
 			List<TList> lists = trelloApi.getBoardLists(b.getId());
@@ -144,6 +214,7 @@ public class Informacao extends JFrame {
 		}
 		return productBacklogList;
 	}
+
 	private void getProjectTime() {
 		List<Board> boards = trelloApi.getMemberBoards(trelloUtilizador);
 		for (Board b : boards) {
@@ -220,61 +291,17 @@ public class Informacao extends JFrame {
 		return sHours;
 	}
 
-	private List<GraficoData> getProjectGraficoData() {
-
-		List<GraficoData> projetoData = new ArrayList<GraficoData>();
-
-		for (SprintHours sprintHours : sHours) {
-			if (sprintHours.hasSpentTime()) {
-				for (Hours hours : sprintHours.getHours()) {
-					int control = 0;
-					for (GraficoData graficoData : projetoData) {
-						if (graficoData.getXData().equals(hours.getUser())) {
-							graficoData.addYData(hours.getSpentTime());
-							control = 1;
-							break;
-						}
-					}
-					if (control == 0)
-						projetoData.add(new GraficoData(hours.getUser(), hours.getSpentTime()));
-				}
-			}
-		}
-
-		return projetoData;
-	}
-
-	private List<GraficoData> getSprintEstimateTimeGraficoData(SprintHours sprintHours) {
-
-		List<GraficoData> sprintEstimateTimeData = new ArrayList<GraficoData>();
-
-		for (Hours hours : sprintHours.getHours()) {
-			sprintEstimateTimeData.add(new GraficoData(hours.getUser(), hours.getEstimateTime()));
-		}
-
-		return sprintEstimateTimeData;
-	}
-
-	private List<GraficoData> getSprintSpentTimeGraficoData(SprintHours sprintHours) {
-
-		List<GraficoData> sprintSpentTimeData = new ArrayList<GraficoData>();
-
-		for (Hours hours : sprintHours.getHours()) {
-			sprintSpentTimeData.add(new GraficoData(hours.getUser(), hours.getSpentTime()));
-		}
-
-		return sprintSpentTimeData;
-	}
-
-	private JScrollPane CriarTabela(List<SprintHours> sprintHoursList) {
+	private JTable CriarTabela(List<SprintHours> sprintHoursList) {
 		String[] colunas = { "Sprint", "User", "Horas previstas", "Horas usadas" };
 		int numberOfLines = 0;
 		for (SprintHours sH : sprintHoursList) {
 			if (sH.hasSpentTime())
 				numberOfLines += sH.getHours().size();
 		}
-		Object[][] dados = new Object[numberOfLines][4];
-		int line = 0;
+		Object[][] dados = new Object[numberOfLines + 1][4];
+		int line = 1;
+		double estimate = 0;
+		double spent = 0;
 		for (SprintHours sH : sprintHoursList) {
 			if (sH.hasSpentTime()) {
 				for (Hours h : sH.getHours()) {
@@ -282,13 +309,51 @@ public class Informacao extends JFrame {
 					dados[line][1] = h.getUser();
 					dados[line][2] = h.getEstimateTime();
 					dados[line][3] = h.getSpentTime();
+					estimate += h.getEstimateTime();
+					spent += h.getSpentTime();
 					line++;
 				}
 			}
 		}
+
+		dados[0][0] = "Projeto";
+		dados[0][1] = "Global";
+		dados[0][2] = estimate;
+		dados[0][3] = spent;
+
 		JTable tabela = new JTable(dados, colunas);
-		JScrollPane jScrollPane = new JScrollPane(tabela);
-		return jScrollPane;
+		tabela.setBackground(UIManager.getColor("Button.light"));
+		return tabela;
 	}
 
+	private JTable criarTabela(List<SprintHours> sprintHoursList, double custoHora) {
+		String[] colunas = { "Sprint", "User", "Pagamento" };
+
+		int numberOfLines = 0;
+		for (SprintHours sH : sprintHoursList) {
+			if (sH.hasSpentTime())
+				numberOfLines += sH.getHours().size();
+		}
+		Object[][] dados = new Object[numberOfLines + 1][3];
+		int line = 1;
+		double custo = 0;
+		for (SprintHours sH : sprintHoursList) {
+			if (sH.hasSpentTime()) {
+				for (Hours h : sH.getHours()) {
+					dados[line][0] = sH.getSprint();
+					dados[line][1] = h.getUser();
+					dados[line][2] = h.getSpentTime() * custoHora;
+					custo += h.getSpentTime() * custoHora;
+					line++;
+				}
+			}
+		}
+		dados[0][0] = "Projeto";
+		dados[0][1] = "Global";
+		dados[0][2] = custo;
+
+		JTable tabela = new JTable(dados, colunas);
+		tabela.setBackground(UIManager.getColor("Button.light"));
+		return tabela;
+	}
 }
